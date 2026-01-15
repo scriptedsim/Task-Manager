47cd193b18bef7048737b76ced1f7e6bb7f5c2ae
scriptedsim <simeenayman22@gmail.com>
2026-01-15 23:40:55 +0530

    adding todos

--- gui.py
+++ gui.py
@@ -2,10 +2,23 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My Todo App", layout=[[label], [input_box, add_button]])
-window.read()
-print("hello")
+window = sg.Window("My Todo App", layout=[[label], [input_box, add_button]], font=("Helvetica", 15))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values["todo"] + '\n'
+            print(new_todo)
+            todos.append(new_todo)
+            functions.write(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
