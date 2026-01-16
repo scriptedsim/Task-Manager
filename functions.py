faeff3256c4e975c0f81ca2b3e3ae2c926f59f89
scriptedsim <simeenayman22@gmail.com>
2026-01-17 00:02:28 +0530

    Edit feature

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
-def get_todos(filepath="todos/todos.txt"):
+def get_todos(filepath="todos.txt"):
     """ Read a text file and return a list of todo items """
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
-def write(todos_arg, filepath="todos/todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
      with open(filepath, 'w') as file:
             file.writelines(todos_arg)
