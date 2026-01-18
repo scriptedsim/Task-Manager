2416f438eec33c2556f7bd6d1778bbf7e512b8b0
scriptedsim <simeenayman22@gmail.com>
2026-01-18 11:08:45 +0530

    implemented 'complete' and 'exit' button

--- gui.py
+++ gui.py
@@ -4,10 +4,16 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values= functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
+list_box = sg.Listbox(values= functions.get_todos(), key="todos", enable_events=True, size=(30, 10))
 edit_button = sg.Button("Edit")
 
-window = sg.Window("My Todo App", layout=[[label], [input_box, add_button], [list_box, edit_button]], font=("Helvetica", 15))
+exit_button = sg.Button("Exit")
+complete_button = sg.Button("Complete")
+
+window = sg.Window("My Todo App", layout=[[label],
+                                          [input_box, add_button],
+                                          [list_box, edit_button, complete_button],
+                                          [exit_button]], font=("Helvetica", 15))
 while True:
     event, values = window.read()
     print(event)
@@ -15,11 +21,12 @@ while True:
     match event:
         case 'Add':
             todos = functions.get_todos()
-            new_todo = values["todo"] + '\n'
+            new_todo = values["todo"]
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-        
+            window["todo"].update(value="")
+
         case 'Edit':
             todo_to_edit = values["todos"][0]
             new_todo = values["todo"] + '\n'
@@ -30,9 +37,20 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
-
         case 'todos':
             window["todo"].update(value=values["todos"][0])
+
+        case "Exit":
+            break
+
+        case "Complete":
+            todos = functions.get_todos()
+            todo_to_complete = values["todos"][0]
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window["todo"].update(value="")
+
 
         case sg.WIN_CLOSED:
             break
