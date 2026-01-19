46fbef62ca5fa9ef2fd0305794d3817cb327cb76
scriptedsim <simeenayman22@gmail.com>
2026-01-19 22:31:50 +0530

    Web page - Adding todo

--- web.py
+++ web.py
@@ -2,9 +2,23 @@ import streamlit as st
 from functions import get_todos, write_todos
 
 st.title("My todo app")
-st.text_input(label="", placeholder="Add new todo . . .")
+
+def add_todos():
+    # Fetch the value from session state
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    write_todos(todos)
+
+
+st.text_input(label="Add a todo", 
+              label_visibility="collapsed",
+              placeholder="Add new todo . . .", 
+              key="new_todo", 
+              on_change=add_todos)
 
 todos = get_todos()
 for todo in todos:
     st.checkbox(todo)
 
+print("Script execution finished (hello)")
+st.session_state
